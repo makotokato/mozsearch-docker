c65b0304a586fd676d04a0b6417abd3ea8f15a92
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2021-04-16 00:52:53 +0900

    Init

---
+++ web-server/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#/vagrant/infrastructure/web-server-setup.sh /mozsearch-mozilla config3.json /mozilla-index /www
+#/vagrant/infrastructure/web-server-run.sh /mozsearch-mozilla /mozilla-index /www
+/vagrant/infrastructure/web-server-setup.sh $1 $2 $3 $4
+/vagrant/infrastructure/web-server-run.sh $1 $3 $4
+/etc/init.d/nginx start
+tail -f /dev/null
